346a6fa979ba028dad560a7f69146721d1e9c8a2
SS56Bo <sohamsarkar345@gmail.com>
2025-01-27 17:55:40 +0530

    FizzBuzz: User Input

--- hello.go
+++ hello.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	num := 89
+	var num int
+	fmt.Print("Enter a number: ")
+	fmt.Scan(&num)
 	if num%5 == 0 {
 		fmt.Println("Fizz")
 	} else if num%3 == 0 {
