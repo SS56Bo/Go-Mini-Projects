bfd84bcdfe4f54ba1625f063b0dcad156317afc5
SS56Bo <sohamsarkar345@gmail.com>
2025-02-02 15:51:59 +0530

    Mini Project Directory

--- hello.go
+++ hello.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
+// Function to check if a number is prime
+func checkPrime(num int) bool {
+	if num < 2 {
+		return false
+	}
+	for i := 2; i*i <= num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	var num int
+	var N int
 	fmt.Print("Enter a number: ")
-	fmt.Scan(&num)
-	if num%5 == 0 {
-		fmt.Println("Fizz")
-	} else if num%3 == 0 {
-		fmt.Println("Buzz")
-	} else {
-		fmt.Println("FizzBuzz")
+	_, err := fmt.Scan(&N)
+	if err != nil {
+		fmt.Println("Invalid input.")
+		return
+	}
+
+	fmt.Println("Prime numbers up to", N, "are:")
+	for i := 2; i <= N; i++ {
+		if checkPrime(i) {
+			fmt.Println(i)
+		}
 	}
 }
